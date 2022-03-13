014460bcfcb473dcea054679f67fbaabbf7eb825
aissam-gif <aissamboussoufiane999@gmail.com>
2022-03-13 14:13:40 +0100

    Add template thymeleaf design

---
+++ src/main/java/net/codejava/controller/RegistrationController.java
@@ -0,0 +1,59 @@
+package net.codejava.controller;
+
+import net.codejava.model.User;
+import net.codejava.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+
+        return "signup_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegister(User user, HttpServletRequest request)
+            throws UnsupportedEncodingException, MessagingException {
+        service.register(user, getSiteURL(request));
+        return "register_success";
+    }
+
+    @GetMapping("/verify")
+    public String verifyUser(@Param("code") String code) {
+        if (service.verify(code)) {
+            return "verify_success";
+        } else {
+            return "verify_fail";
+        }
+    }
+
+    @GetMapping("/verify-manager")
+    public String verifyManager(@Param("code") String code) {
+        if (service.verify(code)) {
+            return "verify_success";
+        } else {
+            return "verify_fail";
+        }
+    }
+
+    private String getSiteURL(HttpServletRequest request) {
+        String siteURL = request.getRequestURL().toString();
+        return siteURL.replace(request.getServletPath(), "");
+    }
+
+}
