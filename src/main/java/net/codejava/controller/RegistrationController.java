24bfddb46effc2ff4c22d2a55ef6302a7eb941ab
aissam-gif <aissamboussoufiane999@gmail.com>
2022-03-14 19:14:46 +0100

    Refactoring v1

--- src/main/java/net/codejava/controller/RegistrationController.java
+++ src/main/java/net/codejava/controller/RegistrationController.java
@@ -3,28 +3,26 @@ package net.codejava.controller;
 import lombok.extern.slf4j.Slf4j;
 import net.codejava.model.User;
 import net.codejava.service.UserService;
+import net.codejava.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 @Controller @Slf4j
 public class RegistrationController {
     @Autowired
-    private UserService service;
+    private UserService  service;
 
     @GetMapping("/login")
     public String showLoginForm() {
@@ -33,6 +31,7 @@ public class RegistrationController {
         if (auth != null && !(auth instanceof AnonymousAuthenticationToken)) {
             return "redirect:/home";
         }
+
             return "login_form";
     }
 
@@ -47,20 +46,26 @@ public class RegistrationController {
         return "signup_form";
     }
 
+
     @PostMapping("/process_register")
-    public String processRegister(User user, HttpServletRequest request)
+    public String processRegister(User user, HttpServletRequest request, RedirectAttributes ra)
             throws UnsupportedEncodingException, MessagingException {
         service.register(user, getSiteURL(request));
-        return "register_success";
+        return "register_status";
     }
 
+
+
     @GetMapping("/verify")
-    public String verifyUser(@Param("code") String code) {
+    public String verifyUser(@Param("code") String code, RedirectAttributes ra) {
         if (service.verify(code)) {
-            return "verify_success";
+            ra.addFlashAttribute("message", "Account activated Successfully !");
+            return "redirect:/login";
         } else {
-            return "verify_fail";
+            ra.addFlashAttribute("message", "Sorry, we could not verify account. It maybe already verified, or verification code is incorrect.");
+            return "redirect:/register";
         }
+
     }
 
     @GetMapping("/verify-manager")
